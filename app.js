3feade346a4ee4495492f8b9216a4f6872c72233
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2022-03-27 16:00:19 -0500

    Database connection

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 const express = require("express");
 const app = express();
 const PORT = 8000;
+const mongoose = require("mongoose");
+const { MONGO_URI } = require("./keys");
 
-// Custom Middleware
-const customMiddleware = (req, res, next) => {
-	console.log("Middleware executed");
-	next();
-};
-
-// Routes
-app.get("/", (req, res) => {
-	console.log("home");
-	res.send("Hello World");
+mongoose.connect(MONGO_URI);
+mongoose.connection.on("connected", () => {
+	console.log("connected to mongoDB");
+});
+mongoose.connection.on("error", (err) => {
+	console.log("Error connecting to mongoDB", err);
 });
 
-app.get("/about", customMiddleware, (req, res) => {
-	console.log("about");
-	res.send("about page");
-});
+// // Custom Middleware
+// const customMiddleware = (req, res, next) => {
+// 	console.log("Middleware executed");
+// 	next();
+// };
+
+// // Routes
+// app.get("/", (req, res) => {
+// 	console.log("home");
+// 	res.send("Hello World");
+// });
+
+// app.get("/about", customMiddleware, (req, res) => {
+// 	console.log("about");
+// 	res.send("about page");
+// });
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	MONGO_URI:
+		"mongodb+srv://GAPV-Coder:GAPVdev1990@cluster0.nzbku.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
