ff61a25bb4166625179ec0911a5f0e9a68b654eb
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2022-03-27 18:23:25 -0500

    Add to Middleware and get token

---
+++ middleware/requireLogin.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../keys");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+
+module.exports = (req, res, next) => {
+	const { authorization } = req.headers;
+	// Authorization === Bearer token
+	if (!authorization) {
+		return res.status(401).json({ error: "You must be logged in" });
+	}
+	const token = authorization.replace("Bearer ", "");
+	jwt.verify(token, JWT_SECRET, (err, payload) => {
+		if (err) {
+			return res.status(401).json({ error: "You must be logged in" });
+		}
+		const { _id } = payload;
+		User.findById(_id).then((userData) => {
+			if (!userData) {
+				return res.status(404).json({ error: "User not found" });
+			}
+			req.user = userData;
+			next();
+		});
+	});
+};
